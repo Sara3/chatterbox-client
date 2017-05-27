a07765c0b7433ee3adba77eafdf13e17a091961b
Hack Reactor Student <student@hackreactor.com>
2017-05-27 16:39:37 -0700

    Progress

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,21 +1,27 @@
-//App.js
-var app = {}; 
+
+var app = {
+  server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+  data: '',
+}; 
 
 app.init = function () {
-  var URL = 'http://parse.sfm6.hackreactor.com/';
-  // $.jQuery.ajax(URL, {
-  //   accepts: String, 
-  //   }
+  app.clearMessages();
+  app.fetch();
+  app.handleDropDownChange();
+  app.handleSubmit();
+
 };
 
+//sends object to the server
 app.send = function(message) {
   $.ajax({
-    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    url: app.server,
     type: 'POST',
     data: JSON.stringify(message), 
     contentType: 'application/json',
     success: function(data) {
-      console.log('chatterbox: Messages Sent' + data);
+      console.log('chatterbox: Message Sent ' + data);
+      
     },
     error: function(data) {
       console.log('chatterbox: Didnt Send Message');
@@ -23,55 +29,99 @@ app.send = function(message) {
   });
 };
 
-//app.send('Hoy');
-
+//gets the data from the server
 app.fetch = function() {
   $.ajax({
+    url: app.server,
     type: 'GET',
-    data: JSON.stringify(message), 
+    _data: '', 
     contentType: 'application/json',
-    success: function(data) {
-      console.log('chatterbox: Messages Fetched' + data);
+    success: function(_data) {
+      app.callRenderMessage(_data.results);
+      app.callRenderRoom(_data.results);
+
+      console.log('chatterbox: Messages Fetched' + _data);
+      app.data = _data.results;
     },
-    error: function(data) {
+    error: function(_data) {
       console.log('chatterbox: Didnt Fetch Message');
     }
   });
 };
 
+//removes all the children of chats
 app.clearMessages = function() {
   $('#chats').empty();
 };
 
-app.renderMessage = function(message) {
-  $('#chats').prepend(`<li>  ${message.text}  </li>`);
+//helper fildering the name and the messege 
+app.callRenderMessage = function(arr, r) {
+  app.clearMessages();
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].roomname === r) {
+      app.renderName(arr[i].username);
+      app.renderMessage(arr[i].text);  
+    }
+  }
 };
 
-app.renderRoom = function(roomSelect) {
-  $('#roomSelect').prepend(`<li>  ${roomSelect.text}  </li>`);
+//displays to text screen
+app.renderMessage = function(message) {
+  $('#chats').prepend(`<li>  ${message}  </li>`);
+};
+// displays name to the screen 
+app.renderName = function(message) {
+  $('#chats').prepend(`<li> <strong> ${message} </strong> </li>`);
+};
+
+//getting the roomnames for the data and filtering it
+app.callRenderRoom = function(arr) {
+  var rooms = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].roomname) {
+      rooms.push(arr[i].roomname);
+    }
+  }
+  var r = _.uniq(rooms);
+  for (var i = 0; i < r.length; i++) {
+    app.renderRoom(r[i]);
+  }
+};
+
+//display roomname
+app.renderRoom = function(room) {  
+  $('#roomSelect').append(`<option> ${room} </option>`);
+};
+
+//Submit user message & click button; creates object
+app.handleSubmit = function() {
+  var m = {
+    username: 'Sara',
+    text: '',
+    roomname: 'Dance'
+  };
+  $('#mit').on( 'click', function() {
+    m.text = $('#msgInput').val();
+    app.send(m);
+  });
+};
+
+app.handleDropDownChange = function() {
+//   $('#roomSelect').on('change', function() {
+//     app.clearMessages();
+//     //app.fetch();
+//     //app.callRenderMessage(_.filter(arr, function)
+//     //app.init();
+//     // if ($(this).val() === 'selectionKey') {
+//     //   app.callRenderMessage();
+//     // } 
+//     //app.send(m);
+//   });
 };
 
 app.handleUsernameClick = function() {
-
+  $('mit').on('click', username, _data, function(username) {
+    event.preventDefault();
+    friendList.push(object[key][i]['username']);
+  });
 };
-
-app.handleSubmit = function() {
-
-};
-
-
-//Display messages retrieved from the parse server.
-
-
-// Assign handlers immediately after making the request,
-// and remember the jqxhr object for this request
-
-  
-// Set another completion function for the request above
-
-
-//Setup a way to refresh the displayed messages (either automatically or with a button)
-
-//Allow users to select a user name for themself and to be able to send messages
-
-//protect your app against cross-site scripting (XSS)
